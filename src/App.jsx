eba8463e2577268223c035be074ef3c3612c922b
andrealangianese <andre.langia.01@gmail.com>
2026-04-29 11:10:48 +0200

    barra di ricerca con risultati stampati sotto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chiamata, setChiamata] = useState('')
+  const [risultati, setRisultati] = useState([])
 
+  useEffect(() => {
+    if (chiamata.trim() === '') {
+      setRisultati([])
+      return
+    }
+    fetch(`http://localhost:3333/products?search=${chiamata}`)
+      .then(res => res.json())
+      .then(data => setRisultati(data))
+      .catch(err => console.error(err))
+
+  }, [chiamata])
   return (
-    <h2>ciao</h2>
+    <>
+      <input
+        type="text"
+        placeholder='cerca per brand'
+        value={chiamata}
+        onChange={e => setChiamata(e.target.value)} />
+      {risultati.length > 0 && (<div>
+        {risultati.map(res => (
+          <option key={res.id}>{res.brand}</option>
+        ))}
+      </div>
+      )}
+    </>
   )
 }
 
