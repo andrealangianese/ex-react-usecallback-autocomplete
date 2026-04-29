bafaaa9ef220a38fe036acfbfbd6e990c8fc9785
andrealangianese <andre.langia.01@gmail.com>
2026-04-29 11:48:56 +0200

    aggiunta di debounce e usecallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,55 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+
+//funzione debounce generica per ritardare ricaricamento della page 
+function debounce(callback, delay) {
+  let timeout;
+  return (value) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      callback(value)
+    }, delay)
+  }
+}
 
 function App() {
   const [chiamata, setChiamata] = useState('')
   const [risultati, setRisultati] = useState([])
 
-  useEffect(() => {
-    if (chiamata.trim() === '') {
+  // logica di ricerca
+  const fetchProducts = (query) => {
+    if (query.trim() === '') {
       setRisultati([])
       return
     }
-    fetch(`http://localhost:3333/products?search=${chiamata}`)
+
+    fetch(`http://localhost:3333/products?search=${query}`)
       .then(res => res.json())
       .then(data => setRisultati(data))
-      .catch(err => console.error(err))
+      .catch(err => console.error("Errore API:", err))
+  }
 
-  }, [chiamata])
+  // useCallback assicura che questa funzione rimanga la STESSA tra i render.
+  const debouncedSearch = useCallback(
+    debounce((nextValue) => fetchProducts(nextValue), 300),
+    [] // Array di dipendenze vuoto
+  )
+
+  // Gestiamo il cambiamento dell'input
+  const handleChange = (e) => {
+    const val = e.target.value
+    // Aggiorno input immediatamente 
+    setChiamata(val)
+    // Chiamo versione ritardata per l'API
+    debouncedSearch(val)
+  }
+
   return (
     <>
       <input
         type="text"
         placeholder='cerca per brand'
         value={chiamata}
-        onChange={e => setChiamata(e.target.value)} />
+        onChange={handleChange} />
       {risultati.length > 0 && (<div>
         {risultati.map(res => (
           <option key={res.id}>{res.brand}</option>
